3402f4db0159a3fea772f3ecc28ee0b5b137c2b2
Tushar Sain <skytusharkum786@gmail.com>
2025-02-02 19:42:45 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,17 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const connectToMongo = require("./db");
 const authRoutes = require("./Routes/auth");
-const cors = require("cors");  // Correctly require the cors package
+const cors = require("cors");
 
 const app = express();
+
+// CORS configuration
+app.use(cors({
+  origin: "https://project-beyond.vercel.app",  
+  methods: ["POST", "GET"],                    
+  credentials: true                           
+}));
+
 const PORT = process.env.PORT || 5000;
 
 // Connect to MongoDB
@@ -14,7 +22,6 @@ connectToMongo();
 // Middleware
 app.use(bodyParser.json());
 app.use(express.json());
-app.use(cors()); // Enable CORS for all routes
 
 // Authentication routes
 app.use("/api/auth", authRoutes);
@@ -23,3 +30,4 @@ app.use("/api/auth", authRoutes);
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
