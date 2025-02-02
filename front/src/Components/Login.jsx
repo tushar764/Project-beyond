397833bc1646b090f2182dd2ac0b54f05a61e131
Tushar Sain <skytusharkum786@gmail.com>
2025-02-02 21:02:23 +0530

    Update Login.jsx

--- front/src/Components/Login.jsx
+++ front/src/Components/Login.jsx
@@ -29,7 +29,7 @@ const Login = (props) => {
     }
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/login", {
+      const response = await fetch("https://project-beyond-back.vercel.app/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
